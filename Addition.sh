343794169c4dbebda0ca4fb3ae8cded7c1f128d0
aman09-git <62902992+aman09-git@users.noreply.github.com>
2024-09-18 20:11:20 +0530

    Create Addition.sh
    Addition in shell Scripting

---
+++ Addition.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#purpose: Addition of numbers
+#Vesion: 1.0
+#Created date: 18th Sep 2024
+#Modified date:
+#Authod: Aman Srivastava
+
+
+echo "addition of A+B"
+echo "Enter value of A"
+read A
+echo "Enter value of B"
+read B
+echo "A+B= $A+$B = $[ A + B ]"
+
+
+#End
+#
+~
+~
+~
+~
+~
